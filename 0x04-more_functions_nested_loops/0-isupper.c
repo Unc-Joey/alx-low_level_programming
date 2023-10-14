3b0d97977e761dd3661113f870ec7f6a8071c7fd
Unc-Joey <josephsekhukhune98@gmail.com>
2023-10-13 18:11:20 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,12 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);  /* c is uppercase */
+	}
+	else
+	{
+		return (0);  /* c is not uppercase */
+	}
 }
