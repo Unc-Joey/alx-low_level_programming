dd3270f5f23ee4cd99d22ac492a74a90417ccfe1
Unc-Joey <josephsekhukhune98@gmail.com>
2023-10-23 17:45:19 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to search in.
+ * @needle: The substring to locate.
+ *
+ * Return: A pointer to the beginning of the located substring, or NULL if
+ * the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
+
